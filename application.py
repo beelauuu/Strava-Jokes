df9bff9b7bfad5856f1413e1a5e77295a700fdf8
beelauuu <103338659+beelauuu@users.noreply.github.com>
2022-07-06 21:53:41 -0400

    Update application.py

--- application.py
+++ application.py
@@ -6,12 +6,15 @@ from flask import Flask
 from flask_restful import reqparse
 from flask import jsonify
 
+#Basic Flask App
 app = Flask(__name__)
 
+#Home Page (just to make sure the application is up and running)
 @app.route('/')
 def api_root():
     return 'Welcome guys to the Strava App'
 
+#/strava route to listen for any new activity and run the joke whenever an activity is uploaded
 @app.route('/strava', methods=['POST'])
 def api_strava_message():
     if request.headers['Content-Type'] == 'application/json':
